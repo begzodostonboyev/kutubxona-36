8134e824ea0187fc33743da4e8a71cdfc58a6220
begiostonboyev <ostonboyevbegzod@gmail.com>
2025-09-07 12:53:14 +0500

    first commint

--- main/views.py
+++ main/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from django.urls import path
 
 # Create your views here.
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def mualliflar_view(request):
+    mualliflar = Muallif.objects.all()
+    context={
+        'mualliflar': mualliflar,
+    }
+    return render(request, 'mualliflar.html',context)
+
+def tanlangan_muallif_view(request, muallif_id):
+    t_muallif = Muallif.objects.get(id=muallif_id)
+    context = {
+        't_muallif': t_muallif,
+    }
+    return render(request,'muallif_malumoti.html',context)
+
+def kitoblar_view(request):
+    kitoblar = Kitob.objects.all()
+    context = {
+        'kitoblar': kitoblar,
+    }
+    return render(request,'3topshiriq.html',context)
+
